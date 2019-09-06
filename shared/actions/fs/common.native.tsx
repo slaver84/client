9a9b42a69eb95a03ddd589797e6708171a782171
Song Gao <song@gao.io>
2019-09-05 22:49:52 -0700

    O_O

--- shared/actions/fs/common.native.tsx
+++ shared/actions/fs/common.native.tsx
@@ -1,8 +1,8 @@
 import logger from '../../logger'
 import * as FsGen from '../fs-gen'
 import * as Types from '../../constants/types/fs'
+import * as Constants from '../../constants/fs'
 import * as Saga from '../../util/saga'
-import * as Flow from '../../util/flow'
 import {TypedState} from '../../constants/reducer'
 import {parseUri, launchImageLibraryAsync} from '../../util/expo-image-picker'
 import {makeRetriableErrorHandler} from './shared'
@@ -20,33 +20,32 @@ const pickAndUploadToPromise = (_: TypedState, action: FsGen.PickAndUploadPayloa
     )
     .catch(makeRetriableErrorHandler(action))
 
-const downloadSuccess = (state: TypedState, action: FsGen.DownloadSuccessPayload) => {
-  const {key, mimeType} = action.payload
-  const download = state.fs.downloads.get(key)
-  if (!download) {
-    logger.warn('missing download key', key)
+const finishedDownloadWithIntent = (state: TypedState, action: FsGen.FinishedDownloadWithIntentPayload) => {
+  const {downloadID, downloadIntent, mimeType} = action.payload
+  const downloadState = state.fs.downloads.state.get(downloadID, Constants.emptyDownloadState)
+  if (downloadState === Constants.emptyDownloadState) {
+    logger.warn('missing download', downloadID)
     return
   }
-  const {intent, localPath} = download.meta as Types.DownloadMeta
-  switch (intent) {
+  const {localPath} = downloadState
+  switch (downloadIntent) {
     case Types.DownloadIntent.CameraRoll:
       return saveAttachmentDialog(localPath)
-        .then(() => FsGen.createDismissDownload({key}))
+        .then(() => FsGen.createDismissDownload({downloadID}))
         .catch(makeRetriableErrorHandler(action))
     case Types.DownloadIntent.Share:
       // @ts-ignore codemod-issue probably a real issue
       return showShareActionSheetFromURL({mimeType, url: localPath})
-        .then(() => FsGen.createDismissDownload({key}))
+        .then(() => FsGen.createDismissDownload({downloadID}))
         .catch(makeRetriableErrorHandler(action))
     case Types.DownloadIntent.None:
       return
     default:
-      Flow.ifFlowComplainsAboutThisFunctionYouHaventHandledAllCasesInASwitch(intent)
       return undefined
   }
 }
 
 export default function* nativeSaga(): Saga.SagaGenerator<any, any> {
   yield* Saga.chainAction2(FsGen.pickAndUpload, pickAndUploadToPromise)
-  yield* Saga.chainAction2(FsGen.downloadSuccess, downloadSuccess)
+  yield* Saga.chainAction2(FsGen.finishedDownloadWithIntent, finishedDownloadWithIntent)
 }
